d24ef9bc4267381d7dc7610789caf9393d6d96c6
Jorge Islas <jorge.islas.2024@gmail.com>
2024-03-16 09:40:38 -0600

    Agregar resolución de problema de programación lineal para optimizar recursos

--- ganancias.py
+++ ganancias.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
 
+def obtener_ventas(precios_de_venta_items):
+    coeficientes_de_venta = 0.92 * precios_de_venta_items
+    return coeficientes_de_venta
+
 def obtener_funcion_ganancias(
     precios_de_venta_items,
     cuota_de_creacion_por_unidad,
     precios_de_compra_materiales
 ):
-    coeficientes_items = 0.92 * precios_de_venta_items - cuota_de_creacion_por_unidad
+    coeficientes_venta = obtener_ventas(precios_de_venta_items)
+    coeficientes_items = coeficientes_venta - cuota_de_creacion_por_unidad
     coeficientes_de_funcion = np.hstack(
         (coeficientes_items, -precios_de_compra_materiales)
     )
--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import json
 import pandas as pd
 import numpy as np
+from scipy.optimize import linprog
 from cargarDatos import cargar_item_db
 from restriccionesMateriales import obtener_resticciones_de_materiales
 from restriccionDinero import obtener_restriccion_dinero
 from Restricciones import obtener_restricciones
 from llamadasAPI import obtener_precios_compra_materiales, obtener_precios_venta_items
-from ganancias import obtener_funcion_ganancias
+from ganancias import obtener_funcion_ganancias, obtener_ventas
 
 item_db = cargar_item_db()
 
 # Luego podrá cambiar esta lista con filtros y selecciones
 # de objetos
 lista_de_items = list(item_db.keys())
-lista_de_items = [
-    "T2_MAIN_SWORD",
-    "T3_BAG",
-    "T3_MAIN_CURSEDSTAFF"
-]
 
 # Despues importar la lista de materiales
 # de algún otro lado
@@ -33,7 +29,7 @@ cuotas_de_estaciones = {
     "Forja del guerrero": 2400,
     "Cabaña del cazador": 2390,
     "Torre del mago": 2360,
-    "Fábrica de herramientas": 2120
+    "Fábrica de herramientas": 2390
 }
 
 # Presupuesto de dinero para la operación
@@ -45,6 +41,9 @@ ciudad = "FortSterling"
 # Obtener restricciones de materiales y listas ordenadas
 # de items y materiales
 restricciones_de_materiales, lista_items_ordenados, lista_materiales_ordenados = obtener_resticciones_de_materiales(lista_de_items, materiales)
+
+print(f"Items considerados: {len(lista_items_ordenados)}")
+print(f"Materiales considerados: {len(lista_materiales_ordenados)}")
 
 precios_de_venta_items = obtener_precios_venta_items(
     lista_items_ordenados, ciudad
@@ -63,9 +62,6 @@ matriz_restricciones, vector_restricciones = obtener_restricciones(
     presupuesto
 )
 
-print(matriz_restricciones.shape)
-print(vector_restricciones.shape)
-
 # Obtener el vector de la función a optimizar
 # (0.92 * ventas - cuotas_de_crafting) * cantidad_items
 # - precios_de_compra_materiales * cantidad_materiales
@@ -73,5 +69,27 @@ funcion_ganancias = obtener_funcion_ganancias(
     precios_de_venta_items, cuota_de_creacion_por_unidad, precios_de_compra_materiales
 )
 
-print(funcion_ganancias.shape)
+# Resolver problema de programación lineal
+c = -funcion_ganancias
+A_ub = matriz_restricciones
+b_ub = vector_restricciones
 
+resultado = linprog(c, A_ub, b_ub, integrality=1, bounds=(0,None))
+
+print("\nCantidades óptimas:")
+for indice in np.where(resultado.x > 0.9)[0]:
+    id_objeto = (lista_items_ordenados + lista_materiales_ordenados)[indice]
+    cantidad_item = resultado.x[indice]
+    print(f"  {id_objeto}: {cantidad_item}")
+
+print(f"\nPresupuesto inicial: {presupuesto}")
+cantidad_items = resultado.x[:len(lista_items_ordenados)]
+cantidad_materiales = resultado.x[len(lista_items_ordenados):]
+ventas = obtener_ventas(precios_de_venta_items) @ cantidad_items
+cuotas_creacion = cuota_de_creacion_por_unidad @ cantidad_items
+compra_materiales = precios_de_compra_materiales @ cantidad_materiales
+print(f"Inversión: {cuotas_creacion + compra_materiales}")
+print(f"  Cuotas creación: {cuotas_creacion}")
+print(f"  Compra materiales: {compra_materiales}")
+print(f"Ventas: {ventas}")
+print(f"Ganancia total: {-resultado.fun}")
